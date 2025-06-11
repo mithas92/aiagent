1c3bd8c1519be993a57c2191d1458ae4a61d9b58
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-06-11 11:32:30 -0500

    Lesson 6 verbose

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
 
@@ -10,22 +11,35 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     if len(sys.argv) > 1:
-        prompt_string = sys.argv[1]
+        user_prompt = sys.argv[1]
     else: 
-        print("Usage: python main prompt_string")
+        print("Usage: python main 'Prompt'")
         sys.exit(1)
+
+
+
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)]
+        ),
+    ]
 
     client  = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=prompt_string
+        contents=messages
     )
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print (f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
 
+    print("Response:")
     print(response.text)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")      
+      
 
 
 if __name__ == "__main__":
