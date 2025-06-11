183bebdc64d21a737575d6b9d08b9ce694776d26
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-06-11 07:57:29 -0500

    Lesson 4

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 # Testing out Gemini Connection
 import os
+import sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
 
-client  = genai.Client(api_key=api_key)
+def main():
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-print(response.text)
+    if len(sys.argv) > 1:
+        prompt_string = sys.argv[1]
+    else: 
+        print("Usage: python main prompt_string")
+        sys.exit(1)
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")      
+    client  = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=prompt_string
+    )
+
+    print(response.text)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")      
+
+
+if __name__ == "__main__":
+    main()
