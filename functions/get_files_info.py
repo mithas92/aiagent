f6142374ba5da1e8fb5b8e92f0a3ad702cff155c
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-11-08 10:06:12 -0600

    Ch 3 L2, Function Dec

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from google import genai
+from google.genai import types
 
 def get_files_info(working_directory, directory=None):
     """
@@ -37,3 +39,27 @@ def get_files_info(working_directory, directory=None):
 
     except Exception as e:
         return f'Error: {e}'
+    
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+
 from google.genai import types
 
 from prompts import system_prompt
+from functions.get_files_info import available_functions
 
 model_name = 'gemini-2.0-flash-001'
 
@@ -22,8 +24,6 @@ def main():
         print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
-
-
     messages = [
         types.Content(
             role="user",
@@ -36,18 +36,20 @@ def main():
     response = client.models.generate_content(
         model=model_name, 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],system_instruction=system_prompt)
     )
     if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
         print (f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
 
-    print("Response:")
-    print(response.text)
-
-      
-
+    if response.function_calls:
+        for i in response.function_calls:
+           print(f"Calling function: {i.name}({i.args})")
+    else:
+        print("Response", response.text)    
+  
 
 if __name__ == "__main__":
     main()
